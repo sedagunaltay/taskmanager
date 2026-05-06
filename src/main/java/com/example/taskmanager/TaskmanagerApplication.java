6e980c07d0065224bec540a06de959802351bcf8
sedagunaltay <gunaltayseda7@gmail.com>
2026-05-06 11:37:25 +0300

    modified pom.xml and Created TaskRequest

--- src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -1,4 +1,4 @@
-package com.example.taskmanager;
+package org.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
