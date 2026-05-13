8038828f2e1da9f6dd8748f47671ffa0f98836ce
sedagunaltay <gunaltayseda7@gmail.com>
2026-05-13 11:13:03 +0300

    TaskController is Update for Devired Query Methods

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,4 +1,4 @@
-package org.example.taskmanager.repository;
+package com.example.taskmanager.repository;
 
 import org.example.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
